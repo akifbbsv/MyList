a2b2d1714bbd420d29df11af68929b02b017b14b
Asus <Asus@DESKTOP-JETP3H0>
2023-01-17 02:37:33 +0400

    Add project files.

---
+++ ConsoleApp7/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+namespace ConsoleApp7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            MyList<string> isimler = new MyList<string>();
+            isimler.Add("Engin");
+            isimler.Add("Kerem");
+            Console.WriteLine(isimler.Count);
+            foreach (var isim in isimler.Items)
+            {
+                Console.WriteLine(isim);
+            }
+        }
+    }
+
+}
